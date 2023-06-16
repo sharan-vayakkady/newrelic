38807f9637b62e9cc6561aa65ecb604e9ebc63a9
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-06-16 14:14:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,9 +30,12 @@ resource "newrelic_synthetics_monitor" "flipkart_com_monitor" {
 }
 
 # Create a notification channel for Slack
-resource "newrelic_synthetics_monitor_channel" "slack_channel" {
-  monitor_id = newrelic_synthetics_monitor.flipkart_com_monitor.id
-  channel_id = newrelic_alert_channel.slack_channel.id
+resource "newrelic_alert_channel" "slack_channel" {
+  name   = "slack-channel"
+  type   = "slack"
+  config = {
+    webhook_url = "https://hooks.slack.com/services/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"
+  }
 }
 
 # Create an alert policy for the monitor
@@ -42,20 +45,22 @@ resource "newrelic_alert_policy" "monitor_failure_policy" {
 }
 
 # Create an alert condition for the monitor
-resource "newrelic_synthetics_alert_condition" "monitor_failure_condition" {
+resource "newrelic_alert_condition" "monitor_failure_condition" {
   policy_id   = newrelic_alert_policy.monitor_failure_policy.id
-  monitor_id  = newrelic_synthetics_monitor.flipkart_com_monitor.id
   name        = "Monitor Failure"
   enabled     = true
-  metric      = "duration"
+  type        = "static"
+  entities {
+    name  = newrelic_synthetics_monitor.flipkart_com_monitor.name
+    type  = "Monitor"
+  }
   
-  violation_time_limit = 5
-
   terms {
     duration          = 1
     priority          = "critical"
     operator          = "above"
     threshold         = 0
+    time_function     = "all"
     waiting_duration  = 0
   }
 }
