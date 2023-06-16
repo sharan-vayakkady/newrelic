1d44d37ed4d50c035053f103b26142b865903de4
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-06-16 16:36:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,43 +21,36 @@ variable "newrelic_api_key" {
   description = "API key for New Relic"
 }
 
-# Configure the New Relic provider
 provider "newrelic" {
-  api_key    = var.newrelic_api_key
-  account_id = "3954397"
+  api_key = var.newrelic_api_key
 }
 
-# Create a synthetic monitor
-resource "newrelic_synthetics_monitor" "flipkart_com_monitor" {
-  name          = "flipkart1.com ping"
-  type          = "SIMPLE"
-  frequency     = 15
-  uri           = "https://www.flipkart1.com"
-  locations     = ["AWS_US_WEST_1"]
-  status        = "ENABLED"
-  sla_threshold = 7.0
+variable "newrelic_api_key" {
+  description = "API key for New Relic"
 }
 
-# Configure email notification channel using New Relic REST API
-provider "http" {}
+resource "newrelic_synthetics_monitor" "example_monitor" {
+  name        = "Example Monitor"
+  frequency   = 15
+  uri         = "https://www.example.com"
+  locations   = ["AWS_US_WEST_2"]
+  status      = "ENABLED"
+  sla         = 5
+  type        = "SIMPLE"
+  security_policies = ["TRUSTED_CERTIFICATE"]
+}
 
-data "http" "create_notification_channel" {
-  url = "https://synthetics.newrelic.com/synthetics/api/v3/monitors/${newrelic_synthetics_monitor.flipkart_com_monitor.id}/notification-channels"
-  method  = "POST"
-  headers = {
-    "Api-Key"       = var.newrelic_api_key
-    "Content-Type"  = "application/json"
+resource "newrelic_synthetics_notification_channel" "example_channel" {
+  name      = "Example Channel"
+  type      = "Email"
+  config    = {
+    recipients = "sharan.vayakkady@gmail.com"
   }
-  body = jsonencode({
-    name = "Email Notification Channel"
-    type = "email"
-    configuration = {
-      recipients = "sharan.vayakkady@gmail.com"
-    }
-  })
 }
 
-# Apply the configuration
-output "notification_channel_response" {
-  value = data.http.create_notification_channel.body
+resource "newrelic_synthetics_monitor_channel" "example_monitor_channel" {
+  monitor_id            = newrelic_synthetics_monitor.example_monitor.id
+  channel_ids           = [newrelic_synthetics_notification_channel.example_channel.id]
+  types                 = ["ALERT"]
+  escalation_policy_ids = []
 }
