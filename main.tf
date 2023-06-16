31546b6b1ef1380f9a624922edf3e057ec2aef60
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-06-16 11:44:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+# Configure the New Relic provider
+provider "newrelic" {
+  api_key = "92580C3C0C3CA75918151D8CACCB6A301946C4935E9C6E9D5189A6A19F7FA5CD"
+}
+
+# Create a synthetic monitor
+resource "newrelic_synthetics_monitor" "sharan_com_monitor" {
+  name              = "sharan.com ping"
+  type              = "SIMPLE"
+  frequency         = 15
+  uri               = "https://www.sharan.com"
+  locations         = ["AWS_US_WEST_1"]
+  status            = "ENABLED"
+  sla_threshold     = 7.0
+  sla_failures_only = true
+}
