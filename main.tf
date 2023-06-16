9eae9f28bfbbf8d2d3b898aa0e844d162ba939cf
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-06-16 14:25:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,62 +1,46 @@
-terraform {
-  required_providers {
-    newrelic = {
-      source  = "newrelic/newrelic"
-      version = "2.15.0"
-    }
-  }
-  backend "s3" {
-    bucket = "ssv-github"
-    key    = "newrelic/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# Provider configuration
 
-# Configure the New Relic provider
 provider "newrelic" {
-  api_key    = "NRAK-P5ZRWJG8TH6LV2D9YQQT8AJ72PL"
-  account_id = "3954397"
+  api_key = "NRAK-P5ZRWJG8TH6LV2D9YQQT8AJ72PL"
 }
 
-# Create a synthetic monitor
+# Resource: New Relic Synthetics Monitor
+
 resource "newrelic_synthetics_monitor" "flipkart_com_monitor" {
-  name          = "flipkart1.com ping"
-  type          = "SIMPLE"
-  frequency     = 15
-  uri           = "https://www.flipkart1.com"
-  locations     = ["AWS_US_WEST_1"]
-  status        = "ENABLED"
-  sla_threshold = 7.0
+  name           = "Flipkart.com"
+  type           = "SIMPLE"
+  frequency      = 15
+  uri            = "https://www.flipkart.com/"
+  locations      = ["AWS_US_WEST_2"]
+  status         = "ENABLED"
+  sla_threshold  = 7
+  follow_redirects = false
 }
 
-# Create a notification channel for Slack
+# Resource: New Relic Alert Channel (Slack)
+
 resource "newrelic_alert_channel" "slack_channel" {
-  name = "slack-channel"
-  type = "slack"
-  config {
-    api_key = "https://hooks.slack.com/services/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"
-  }
+  name     = "Slack Channel"
+  type     = "slack"
+  enabled  = true
+  api_url  = "https://hooks.slack.com/services/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"
 }
 
-# Create an alert policy for the monitor
+# Resource: New Relic Alert Policy
+
 resource "newrelic_alert_policy" "monitor_failure_policy" {
-  name                  = "Monitor Failure"
-  incident_preference   = "PER_POLICY"
+  name     = "Monitor Failure Policy"
+  incident_preference = "PER_POLICY"
 }
 
-# Create an alert condition for the monitor
+# Resource: New Relic Alert Condition
+
 resource "newrelic_alert_condition" "monitor_failure_condition" {
   policy_id = newrelic_alert_policy.monitor_failure_policy.id
   name      = "Monitor Failure"
   enabled   = true
   type      = "static"
-
-  entities = [
-    {
-      name = newrelic_synthetics_monitor.flipkart_com_monitor.name
-      type = "Monitor"
-    }
-  ]
+  metric    = "apdex"
 
   term {
     duration      = "5"
@@ -65,4 +49,17 @@ resource "newrelic_alert_condition" "monitor_failure_condition" {
     threshold     = "0"
     time_function = "all"
   }
+
+  entities = [
+    {
+      name = "${newrelic_synthetics_monitor.flipkart_com_monitor.name}"
+      type = "Monitor"
+    }
+  ]
+}
+
+# Output: Monitor ID
+
+output "monitor_id" {
+  value = newrelic_synthetics_monitor.flipkart_com_monitor.id
 }
