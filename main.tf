a9fd99e9762fd567951d207dcc73e37c7146a758
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-06-18 14:11:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,67 +2,65 @@ terraform {
   required_providers {
     newrelic = {
       source  = "newrelic/newrelic"
-      version = "2.15.0"
+      version = "3.24.2"
     }
+  }
+  backend "s3" {
+    bucket = "ssv-github"
+    key    = "newrelic/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
+# Configure the New Relic provider
 provider "newrelic" {
-  api_key    = var.newrelic_api_key
-  account_id = 3954397
-}
-variable "newrelic_api_key" {
-  description = "API key for New Relic"
+  api_key    = "NRAK-P5ZRWJG8TH6LV2D9YQQT8AJ72PL"
+  account_id = "3954397"
 }
 
-resource "newrelic_synthetics_monitor" "flipkart_monitor" {
-  name          = "flipkart1.com ping"
+# Create a synthetic monitor
+resource "newrelic_synthetics_monitor" "amazon_com_monitor" {
+  name          = "amazon.com ping"
   type          = "SIMPLE"
   frequency     = 15
-  uri           = "https://www.flipkart1.com"
+  uri           = "https://www.amazon.com"
   locations     = ["AWS_US_WEST_1"]
   status        = "ENABLED"
   sla_threshold = 7.0
 }
 
-resource "newrelic_alert_channel" "email_alert" {
-  name   = "Email Alert"
-  type   = "email"
-
-  config {
-    recipients = "sharan.vayakkady@gmail.com"
+# Create a notification channel for Slack
+resource "newrelic_alert_channel" "slack_channel" {
+  name   = "slack-channel"
+  type   = "slack"
+  slack {
+    webhook_url = "https://hooks.slack.com/services/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"
   }
 }
 
-resource "newrelic_alert_policy" "flipkart_monitor_policy" {
-  name                = "Flipkart Monitor Policy"
-  incident_preference = "PER_POLICY"
+# Create an alert policy for the monitor
+resource "newrelic_alert_policy" "monitor_failure_policy" {
+  name                 = "Monitor Failure"
+  violation_time_limit = 5
+  evaluation_offset    = 0
 }
 
-resource "newrelic_alert_condition" "flipkart_monitor_condition" {
-  name                 = "Flipkart Monitor Condition"
-  policy_id            = newrelic_alert_policy.flipkart_monitor_policy.id
-  type                 = "monitor"
-  enabled              = true
-  violation_time_limit = 10
+resource "newrelic_alert_condition" "monitor_failure_condition" {
+  policy_id = newrelic_alert_policy.monitor_failure_policy.id
 
-  entities = [
-    {
-      name            = newrelic_synthetics_monitor.flipkart_monitor.name
-      type            = "monitor"
-      condition_scope = "apps"
-    }
-  ]
+  name    = "Monitor Failure"
+  enabled = true
 
   terms {
-    duration      = 5
-    operator      = "above"
+    duration      = 1
     priority      = "critical"
+    operator      = "above"
     threshold     = 0
     time_function = "all"
-
-    notifications {
-      channel_id = newrelic_alert_channel.email_alert.id
-    }
   }
+}
+
+resource "newrelic_alert_channel_policy" "monitor_failure_channel_policy" {
+  policy_id    = newrelic_alert_policy.monitor_failure_policy.id
+  channel_ids = [newrelic_alert_channel.slack_channel.id]
 }
