a8522dc20aad2a694dab2c6a6af2adda047271f2
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-06-18 16:03:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "newrelic_synthetics_monitor" "ping_monitor" {
   name              = "Amazon Ping Monitor"
   type              = "SIMPLE"
   uri               = "https://www.sharan.com"
-  locations_public  = ["AWS_US_WEST_1"]
+  locations         = ["AWS_US_WEST_1"]  # Updated attribute name
   status            = "ENABLED"
 }
 
@@ -31,7 +31,7 @@ resource "newrelic_alert_channel" "email" {
 
   config {
     recipients              = "sharan.vayakkady@gmail.com"
-    include_json_attachment = "1"
+    include_json_attachment = true  # Updated attribute value
   }
 }
 
@@ -44,23 +44,29 @@ resource "newrelic_alert_condition" "ping_monitor_condition" {
   name             = "Ping Monitor Failure"
   policy_id        = newrelic_alert_policy.amazon_alerts.id
   enabled          = true
-  type             = "servers_metric"
+  type             = "static"  # Updated attribute value
   entities         = [newrelic_synthetics_monitor.ping_monitor.id]
   metric           = "failure_rate"
-  term {
-    threshold = 0
-    duration  = 5
-    operator  = "above"
-    priority  = "critical"
-    time_function = "all"
+  violation_time_limit_seconds = 600  # Added attribute
+  terms {
+    threshold            = 0
+    time_function        = "all"
+    duration             = 5
+    operator             = "above"
+    priority             = "critical"
+    waiting_function     = "all"
+    time_aggregation     = "all"
+    evaluation_offset    = 0
+    aggregation_window   = 0
+    ignore_overlap       = true
+    user_defined_metric  = ""
+    metric_type          = "absolute"
+    total_seconds        = 600
   }
 }
 
-
 # Link the channel to the policy
 resource "newrelic_alert_policy_channel" "alert_email" {
-  policy_id  = newrelic_alert_policy.amazon_alerts.id
-  channel_ids = [
-    newrelic_alert_channel.email.id
-  ]
+  policy_id    = newrelic_alert_policy.amazon_alerts.id
+  channel_ids  = [newrelic_alert_channel.email.id]
 }
