d317f5e49e5b8d78ac02a1d9d8751f0ac7b07a85
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-06-18 14:43:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,8 @@
-terraform {
-  required_providers {
-    newrelic = {
-      source  = "newrelic/newrelic"
-      version = "3.24.2"
-    }
-  }
-  backend "s3" {
-    bucket = "ssv-github"
-    key    = "newrelic/terraform.tfstate"
-    region = "us-east-1"
-  }
+# Create an alert policy
+resource "newrelic_alert_policy" "alert" {
+  name = "Your Concise Alert Name"
 }
 
-# Configure the New Relic provider
-provider "newrelic" {
-  api_key    = "NRAK-P5ZRWJG8TH6LV2D9YQQT8AJ72PL"
-  account_id = "3954397"
-}
-
-# Create a synthetic monitor
 resource "newrelic_synthetics_monitor" "amazon_com_monitor" {
   name          = "amazon.com ping"
   type          = "SIMPLE"
@@ -26,21 +10,17 @@ resource "newrelic_synthetics_monitor" "amazon_com_monitor" {
   uri           = "https://www.amazon.com"
   locations     = ["AWS_US_WEST_1"]
   status        = "ENABLED"
-  sla_threshold = 7.0
-}
-
-# Create an alert policy
-resource "newrelic_alert_policy" "alert" {
-  name = "Your Concise Alert Name"
 }
 
 resource "newrelic_alert_condition" "monitor_failure_condition" {
   policy_id = newrelic_alert_policy.alert.id
+  type      = "static"
+  name      = "Monitor Failure"
+  enabled   = true
 
-  name    = "Monitor Failure"
-  enabled = true
+  entities = [newrelic_synthetics_monitor.amazon_com_monitor.id]
 
-  terms {
+  term {
     duration      = 1
     priority      = "critical"
     operator      = "above"
@@ -49,7 +29,7 @@ resource "newrelic_alert_condition" "monitor_failure_condition" {
   }
 }
 
-resource "newrelic_alert_channel" "email" {
+resource "newrelic_notification_channel" "email" {
   name = "email"
   type = "email"
 
@@ -61,8 +41,6 @@ resource "newrelic_alert_channel" "email" {
 
 # Link the channel to the policy
 resource "newrelic_alert_policy_channel" "alert_email" {
-  policy_id  = newrelic_alert_policy.alert.id
-  channel_ids = [
-    newrelic_alert_channel.email.id
-  ]
+  policy_id   = newrelic_alert_policy.alert.id
+  channel_ids = [newrelic_notification_channel.email.id]
 }
