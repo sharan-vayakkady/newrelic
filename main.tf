c1546029c8ccc5b578bad4345657254c212cd418
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-06-18 22:45:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,28 @@ terraform {
     }
   }
 }
+
 provider "newrelic" {
   api_key    = "NRAK-P5ZRWJG8TH6LV2D9YQQT8AJ72PL"
   account_id = 3954397
+}
+
+resource "newrelic_notification_channel" "email_channel" {
+  account_id      = 3954397
+  name            = "email_channel_notification"
+  type            = "EMAIL"
+  destination_id  = "sharan.vayakkady@gmail.com"
+  product         = "IINT"
+
+  property {
+    key   = "Website down"
+    value = "website is down"
+  }
+
+  property {
+    key   = "customDetailsEmail"
+    value = "issue id - {{issueId}}"
+  }
 }
 
 resource "newrelic_synthetics_monitor" "ping_monitor" {
@@ -25,38 +44,18 @@ resource "newrelic_synthetics_monitor" "ping_monitor" {
   verify_ssl                = true
 }
 
-# Create an alert policy
 resource "newrelic_alert_policy" "amazon_alerts" {
   name = "amazon alert"
 }
 
-resource "newrelic_synthetics_alert_condition" "ping_monito_conditionr" {
-  policy_id = newrelic_alert_policy.amazon_alerts.id
-
-  name        = "ping monitor"
-  monitor_id  = newrelic_synthetics_monitor.ping_monitor.id
-  runbook_url = "https://www.example.com"
-}
-
-resource "newrelic_notification_channel" "email_channel" {
-  account_id = 3954397
-  name = "email_channel_notification"
-  type = "EMAIL"
-  destination_id = "sharan.vayakkady@gmail.com"
-  product = "IINT"
-
-  property {
-    key = "Website down"
-    value = "website is down"
-  }
-
-  property {
-    key = "customDetailsEmail"
-    value = "issue id - {{issueId}}"
-  }
+resource "newrelic_synthetics_alert_condition" "ping_monitor_condition" {
+  policy_id    = newrelic_alert_policy.amazon_alerts.id
+  name         = "ping monitor"
+  monitor_id   = newrelic_synthetics_monitor.ping_monitor.id
+  runbook_url  = "https://www.example.com"
 }
 
 resource "newrelic_alert_policy_channel" "email_policy" {
-  policy_id  = newrelic_alert_policy.amazon_alerts.id
-  channel_ids = newrelic_notification_channel.email_channel.id
+  policy_id    = newrelic_alert_policy.amazon_alerts.id
+  channel_ids  = [newrelic_notification_channel.email_channel.id]
 }
