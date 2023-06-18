d6c5a29085e209d09f24fbc37e2d2956714f805a
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-06-18 15:52:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ resource "newrelic_synthetics_monitor" "ping_monitor" {
   name              = "Amazon Ping Monitor"
   type              = "SIMPLE"
   uri               = "https://www.sharan.com"
+  locations_public  = ["AWS_US_WEST_1"]
   status            = "ENABLED"
 }
 
