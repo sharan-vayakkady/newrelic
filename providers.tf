84de4f2183da973b1fedb2a6594d390bd05d0cb4
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-10-08 11:22:56 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region                   = local.region
-  shared_credentials_files = ["~/.aws/config", "~/.aws/credentials"]
-  profile                  = local.profile
+  #shared_credentials_files = ["~/.aws/config", "~/.aws/credentials"]
+  #profile                  = local.profile
   default_tags {
     tags = {
       createdby = "terraform"
