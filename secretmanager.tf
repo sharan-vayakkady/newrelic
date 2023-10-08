af724c43a0ac3b61830b773d7e9882fe0ce95e83
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-10-08 11:38:36 +0530

    Update secretmanager.tf

--- secretmanager.tf
+++ secretmanager.tf
@@ -1,6 +1,6 @@
 resource "aws_secretsmanager_secret" "slack_webhook" {
   description = "Slack webhook for enabling notification"
-  name        = "/ecomm/synthetic-monitor-qa/slack_webhook
+  name        = "/ecomm/synthetic-monitor-qa/slack_webhook"
   tags = {
     createdby = "terraform"
     team      = "devops"
@@ -9,7 +9,7 @@ resource "aws_secretsmanager_secret" "slack_webhook" {
 
 resource "aws_secretsmanager_secret" "newrelic_api" {
   description = "Newrelic api key to enable or disable QA synthetic monitor"
-  name        = "/ecomm/synthetic-monitor-qa/newrelic-api-key
+  name        = "/ecomm/synthetic-monitor-qa/newrelic-api-key"
   tags = {
     createdby = "terraform"
     team      = "devops"
