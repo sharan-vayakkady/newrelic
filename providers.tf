77232e45242e6b21d85051a8820cb589a85f08c9
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-10-10 17:35:26 +0530

    Update and rename bkpproviders.txt to providers.tf

--- providers.tf
+++ providers.tf
@@ -10,14 +10,3 @@ provider "aws" {
   }
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.51.0"
-    }
-  }
-
-  required_version = "= 1.3.7"
-}
-
